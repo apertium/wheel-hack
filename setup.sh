904f5670f2e9677d68245362bde0e4a2cfcc6488
Tino Didriksen <mail@tinodidriksen.com>
2022-12-25 16:38:56 +0000

    Forgot clone

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ ssh -l tino 192.168.1.12 'open -a Docker'
 cat Dockerfile.py38.x86_64_2_28 | perl -wpne 's/x86_64/aarch64/g;' | ssh -l tino 192.168.1.12 '/usr/local/bin/docker build --squash -t wheel-py38-aarch64 -'
 cat Dockerfile.py39.x86_64_2_28 | perl -wpne 's/x86_64/aarch64/g;' | ssh -l tino 192.168.1.12 '/usr/local/bin/docker build --squash -t wheel-py39-aarch64 -'
 cat Dockerfile.py310.x86_64 | perl -wpne 's/amd64/arm64v8/g;' | ssh -l tino 192.168.1.12 '/usr/local/bin/docker build --squash -t wheel-py310-aarch64 -'
+
+cd pkgs
+git clone git@github.com:apertium/tree-sitter-apertium.git
